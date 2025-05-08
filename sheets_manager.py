08110adb038389015d4543984d515bfcae394333
YaroLove <vasyaroslav.24@gmail.com>
2025-05-07 22:40:09 -0700

    Update sheets_manager.py

--- sheets_manager.py
+++ sheets_manager.py
@@ -54,8 +54,6 @@ class SheetsManager:
                 "Image_URL": result["image_url"].split("/")[-1],   # лишаємо тільки файл
             }
             payload = {"path": "Results", "rowData": row_data}
-            print("HTTP status:", r.status_code)
-            print("Body:", r.text[:200])
             print("Sending to sheets:", json.dumps(payload, indent=2))
             r = requests.post(SCRIPT_URL, json=payload, timeout=10)
             return r.text
