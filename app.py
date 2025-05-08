841416bc63fce10a429f9bfd26a7ae2b5041dce6
YaroLove <vasyaroslav.24@gmail.com>
2025-05-07 19:58:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -154,5 +154,5 @@ def submit_analysis():
     except Exception as e:
         return jsonify({'success': False, 'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True) 
+#if __name__ == '__main__':
+   # app.run(host='127.0.0.1', port=5000, debug=True) 
